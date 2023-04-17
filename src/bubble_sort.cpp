9d9228f155933074a57b0eae925be03c5942d4bb
Higor Rozan <higorb.rozan@gmail.com>
2023-04-17 00:50:00 -0300

    :tada: first commit

---
+++ src/bubble_sort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+void bubble_sort(int arr[], int n) {
+  bool swap = true;
+
+  while (swap) {
+    swap = false;
+    for (size_t i = 1; i < n; i++) {
+      if (arr[i - 1] > arr[i]) {
+        int aux = arr[i - 1];
+        arr[i - 1] = arr[i];
+        arr[i] = aux;
+        swap = true;
+      }
+    }
+  }
+}
+
+int main(void) {
+  int arr[] = {5, 7, 2, 9, 1, 4};
+  int n = 6;
+
+  cout << "Unsorted\n";
+  for (size_t i = 0; i < n; i++) {
+    cout << arr[i] << "\n";
+  }
+
+  bubble_sort(arr, n);
+
+  cout << "Sorted\n";
+  for (size_t i = 0; i < n; i++) {
+    cout << arr[i] << "\n";
+  }
+
+  return EXIT_SUCCESS;
+}
